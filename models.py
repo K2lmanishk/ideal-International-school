36e8c79a73b139965303c05ef644274ce80a3c54
k2lmanish <manish865133@gmail>
2026-04-22 19:09:31 +0530

    Fix password_hash length for PostgreSQL and add psycopg2

--- models.py
+++ models.py
@@ -9,7 +9,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128))
+    password_hash = db.Column(db.String(512))   # या db.Column(db.Text)
     role = db.Column(db.String(20), nullable=False)
     full_name = db.Column(db.String(100))
     profile_pic = db.Column(db.String(200), default='default.png')
