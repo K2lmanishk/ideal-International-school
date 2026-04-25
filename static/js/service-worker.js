a3de72426ceb442b6d644f12f32ee266980a6e89
k2lmanish <manish865133@gmail>
2026-04-25 09:28:38 +0530

    Add PWA support: manifest, service worker, installable app

--- static/js/service-worker.js
+++ static/js/service-worker.js
@@ -1,15 +1,11 @@
-const CACHE_NAME = 'college-sms-v1';
+const CACHE_NAME = 'ideal-school-v1';
 const urlsToCache = [
   '/',
   '/static/css/style.css',
   '/static/js/script.js',
-  '/static/images/icon-192.png',
-  '/static/images/icon-512.png',
-  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
-  'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css'
+  '/static/manifest.json'
 ];
 
-// Install service worker
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
@@ -17,22 +13,9 @@ self.addEventListener('install', event => {
   );
 });
 
-// Fetch from cache
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => response || fetch(event.request))
-  );
-});
-
-// Activate and clean old caches
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.filter(name => name !== CACHE_NAME)
-          .map(name => caches.delete(name))
-      );
-    })
   );
 });
